ac44642fc7e4acaad319265b3a9bebaf0b696626
rafael.santos <rafaelsantos32141@gmail.com>
2024-09-16 11:15:21 -0300

    Formatação de código como projeto poetry

--- params.py
+++ params.py
@@ -1,6 +1,9 @@
 folders = ['gorillas', 'orangutans']
-img_size = 64
+img_size = 128
 num_imgs = -1
 test_size = 0.3
 random_state = 42
 max_k = 100
+
+predict_imgs = ['gorilla.jpg', 'gorilla2.jpg',
+                'orangutan.jpg', 'orangutan2.jpg']
